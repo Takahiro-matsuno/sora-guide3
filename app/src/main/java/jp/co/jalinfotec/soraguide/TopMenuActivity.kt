8d3f290a0815126598080f51e7d1b8a520da2e5d
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-09-12 16:50:07 +0900

    first commit

---
+++ app/src/main/java/jp/co/jalinfotec/soraguide/TopMenuActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.jalinfotec.soraguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_menu)
+    }
+}
