6a5f623cb8dce8bd59ca3aba43de852ede2d8c93
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-09-18 13:07:53 +0900

    タクシー予約のソースを整理して追加

---
+++ app/src/main/java/jp/co/jalinfotec/soraguide/Constants.kt
@@ -0,0 +1,13 @@
+package jp.co.jalinfotec.soraguide
+
+object Constants {
+
+    /**
+     * Taxi Reservation
+     */
+    val taxiUrl =
+        if (BuildConfig.DEBUG) "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/app/registration"
+        else "https://taxiapptest.azurewebsites.net/taxiReservation-0.1.2/app/registration"
+    const val taxiUserAgent = "sora-GuideApp"
+
+}
---
+++ app/src/main/java/jp/co/jalinfotec/soraguide/base/BaseNavigationActivity.kt
@@ -0,0 +1,67 @@
+package jp.co.jalinfotec.soraguide.base
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import jp.co.jalinfotec.soraguide.R
+import jp.co.jalinfotec.soraguide.taxi.TaxiActivity
+import kotlinx.android.synthetic.main.activity_navigation.*
+
+abstract class BaseNavigationActivity:
+    AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener
+{
+
+    abstract fun setMainContent()
+
+    abstract fun setToolbarTitle()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+        setToolbarTitle()
+        setMainContent()
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    /**
+     * left navigation選択時の設定
+     */
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_airport -> { // todo 遷移先画面を設定する
+                //startActivity(Intent(this, Activity::class.java))
+                finish()
+            }
+            R.id.nav_flight -> { // todo 遷移先画面を設定する
+                //startActivity(Intent(this, Activity::class.java))
+                finish()
+            }
+            R.id.nav_tourism -> { // todo 遷移先画面を設定する
+                //startActivity(Intent(this, Activity::class.java))
+                finish()
+            }
+            R.id.nav_taxi -> {
+                startActivity(Intent(this, TaxiActivity::class.java))
+                finish()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
