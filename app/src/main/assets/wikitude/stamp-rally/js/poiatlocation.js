721247e87b79e50db74778a9096d12c34485f70a
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-09-25 17:38:16 +0900

    コメントを修正

--- app/src/main/assets/wikitude/stamp-rally/js/poiatlocation.js
+++ app/src/main/assets/wikitude/stamp-rally/js/poiatlocation.js
@@ -4,8 +4,8 @@ var World = {
 	// true once data was fetched
 	initiallyLoadedData: false,
 
-	// POI-Marker asset。マーカーFishの画像
-	markerDrawable_idle: null,
+	// POI-Marker asset スタンプの画像
+	markerDrawable_stamp_red: null,
 
 	// called to inject new POI data。
 	// マーカーを設置する。
@@ -14,16 +14,17 @@ var World = {
 		/*
 			The example Image Recognition already explained how images are loaded and displayed in the augmented reality view. This sample loads an AR.ImageResource when the World variable was defined. It will be reused for each marker that we will create afterwards.
 		*/
-		//画像
-		World.markerDrawable_idle = new AR.ImageResource("assets/stamp_red.png");
+		// 画像
+		World.markerDrawable_stamp_red = new AR.ImageResource("assets/stamp_red.png");
 
 		/*
 			For creating the marker a new object AR.GeoObject will be created at the specified geolocation. An AR.GeoObject connects one or more AR.GeoLocations with multiple AR.Drawables. The AR.Drawables can be defined for multiple targets. A target can be the camera, the radar or a direction indicator. Both the radar and direction indicators will be covered in more detail in later examples.
 		*/
 		// 位置情報
 		var markerLocation = new AR.GeoLocation(poiData.latitude, poiData.longitude, poiData.altitude);
+
 		// 描画情報.８は８倍のサイズ
-		var markerImageDrawable_idle = new AR.ImageDrawable(World.markerDrawable_idle, 8, {
+		var markerImageDrawable_stamp_red = new AR.ImageDrawable(World.markerDrawable_stamp_red, 8, {
 			zOrder: 0,  // 表示順
 			opacity: 1.0,   // 不透明
 			// クリック時処理
@@ -35,7 +36,7 @@ var World = {
 
 		// 回転。アニメーションを定義(１秒間に360度回転)。click時に実行してる
     	var elevatorAnimation = new AR.PropertyAnimation(
-              markerImageDrawable_idle, //the object geoLocation1 holds the animated property
+              markerImageDrawable_stamp_red, //the object geoLocation1 holds the animated property
               "rotation", // the property altitude will be animated
               0, // the start value of the animation
               360, // the resulting value of the animation
@@ -48,7 +49,7 @@ var World = {
 		// AR上に配置
 		var markerObject = new AR.GeoObject(markerLocation, {
 			drawables: {
-				cam: [markerImageDrawable_idle]
+				cam: [markerImageDrawable_stamp_red]
 			}
 		});
 
@@ -58,7 +59,7 @@ var World = {
 	},
 
 	// updates status message shon in small "i"-button aligned bottom center
-	// inde.htmlのメッセージやアイコンに状態を表示する
+	// index.htmlのメッセージやアイコンに状態を表示する
 	updateStatusMessage: function updateStatusMessageFn(message, isWarning) {
 
 		var themeToUse = isWarning ? "e" : "c";
@@ -80,18 +81,18 @@ var World = {
 		/*
 			The custom function World.onLocationChanged checks with the flag World.initiallyLoadedData if the function was already called. With the first call of World.onLocationChanged an object that contains geo information will be created which will be later used to create a marker using the World.loadPoisFromJsonData function.
 		*/
-		//最初だけマーカーの位置を作成する。
+		//　最初だけマーカーの位置を作成する。
 		if (!World.initiallyLoadedData) {
 			// creates a poi object with a random location near the user's location
 			var poiData = {
 				"id": 1,
 				"longitude": (lon + (Math.random() / 5 - 0.1)),
 				"latitude": (lat + (Math.random() / 5 - 0.1)),
-				"altitude": 100.0 //標高
+				"altitude": 100.0 //　標高
 			};
 			//
 			World.loadPoisFromJsonData(poiData);
-			//１回だけのフラグを立てる
+			//　１回だけのフラグを立てる
 			World.initiallyLoadedData = true;
 		}
 	},
