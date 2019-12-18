f79cbd1a1506c2b0630d0ef8a1c13e9ac81dc558
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-12-18 13:14:56 +0900

    タクシー画面のプログレスバーのバグを修正

--- app/src/main/java/jp/co/jalinfotec/soraguide/ui/taxi/TaxiFragment.kt
+++ app/src/main/java/jp/co/jalinfotec/soraguide/ui/taxi/TaxiFragment.kt
@@ -3,7 +3,6 @@ package jp.co.jalinfotec.soraguide.ui.taxi
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +10,7 @@ import android.webkit.CookieManager
 import android.webkit.CookieSyncManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import jp.co.jalinfotec.soraguide.R
 import jp.co.jalinfotec.soraguide.utils.Constants
@@ -19,6 +19,7 @@ import kotlinx.android.synthetic.main.fragment_taxi.*
 class TaxiFragment: Fragment() {
 
     private val logTag = this::class.java.simpleName
+    private var progressBar: ProgressBar? = null
 
     fun newInstance(): TaxiFragment {
         return TaxiFragment()
@@ -31,17 +32,20 @@ class TaxiFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // todo check https://appkitbox.com/knowledge/android/20130819-84
+        progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+
+
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = object: WebViewClient() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                 // ロード開始時
-                progressBar.visibility = View.VISIBLE
+                progressBar?.visibility = View.VISIBLE
             }
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
                 // ロード終了時
-                progressBar.visibility = View.GONE
+                progressBar?.visibility = View.GONE
             }
         }
         // cookieの有効化
@@ -55,10 +59,6 @@ class TaxiFragment: Fragment() {
 
         // user-agent
         webView.settings.userAgentString += Constants.taxiUserAgent
-    }
-
-    override fun onStart() {
-        super.onStart()
         webView.loadUrl(Constants.taxiUrl)
     }
 
